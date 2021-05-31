84ed40a271401b80fb4b9f798df65c22e6283cf0
student <student@localhost>
2021-06-01 00:05:19 +0300

    first commit

---
+++ exchangeRate/Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace exchangeRate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.exchangeDataSet = new exchangeRate.exchangeDataSet();
+            this.exchangeTabBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.exchangeTabTableAdapter = new exchangeRate.exchangeDataSetTableAdapters.exchangeTabTableAdapter();
+            this.tableAdapterManager = new exchangeRate.exchangeDataSetTableAdapters.TableAdapterManager();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.номиАсъорDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.маблагDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.асъориИвазDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.курбDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.натичаDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.санаDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exchangeDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exchangeTabBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(401, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(591, 24);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(114, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(258, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "НОМИ ҚУРБИ АСЪОР";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(401, 70);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(114, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(591, 66);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(114, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(556, 139);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(114, 20);
+            this.textBox5.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(528, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "МАБЛАҒ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(247, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "ҚУРБИ ИВАЗШАВАНДА";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(544, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "ҚУРБ";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(576, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 24);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Натиҷа";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox6.Location = new System.Drawing.Point(55, 54);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(148, 22);
+            this.textBox6.TabIndex = 12;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(86, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Ҷустуҷӯ";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox7.Location = new System.Drawing.Point(55, 85);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(148, 22);
+            this.textBox7.TabIndex = 14;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox8.Location = new System.Drawing.Point(55, 113);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(148, 22);
+            this.textBox8.TabIndex = 15;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(12, 54);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Рӯз";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(7, 85);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 20);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Моҳ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(7, 113);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 20);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Сол";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.номиАсъорDataGridViewTextBoxColumn,
+            this.маблагDataGridViewTextBoxColumn,
+            this.асъориИвазDataGridViewTextBoxColumn,
+            this.курбDataGridViewTextBoxColumn,
+            this.натичаDataGridViewTextBoxColumn,
+            this.санаDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.exchangeTabBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 165);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(740, 312);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // exchangeDataSet
+            // 
+            this.exchangeDataSet.DataSetName = "exchangeDataSet";
+            this.exchangeDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // exchangeTabBindingSource
+            // 
+            this.exchangeTabBindingSource.DataMember = "exchangeTab";
+            this.exchangeTabBindingSource.DataSource = this.exchangeDataSet;
+            // 
+            // exchangeTabTableAdapter
+            // 
+            this.exchangeTabTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.exchangeTabTableAdapter = this.exchangeTabTableAdapter;
+            this.tableAdapterManager.UpdateOrder = exchangeRate.exchangeDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // номиАсъорDataGridViewTextBoxColumn
+            // 
+            this.номиАсъорDataGridViewTextBoxColumn.DataPropertyName = "Номи_Асъор";
+            this.номиАсъорDataGridViewTextBoxColumn.HeaderText = "Номи_Асъор";
+            this.номиАсъорDataGridViewTextBoxColumn.Name = "номиАсъорDataGridViewTextBoxColumn";
+            // 
+            // маблагDataGridViewTextBoxColumn
+            // 
+            this.маблагDataGridViewTextBoxColumn.DataPropertyName = "Маблаг";
+            this.маблагDataGridViewTextBoxColumn.HeaderText = "Маблаг";
+            this.маблагDataGridViewTextBoxColumn.Name = "маблагDataGridViewTextBoxColumn";
+            // 
+            // асъориИвазDataGridViewTextBoxColumn
+            // 
+            this.асъориИвазDataGridViewTextBoxColumn.DataPropertyName = "Асъори_Иваз";
+            this.асъориИвазDataGridViewTextBoxColumn.HeaderText = "Асъори_Иваз";
+            this.асъориИвазDataGridViewTextBoxColumn.Name = "асъориИвазDataGridViewTextBoxColumn";
+            // 
+            // курбDataGridViewTextBoxColumn
+            // 
+            this.курбDataGridViewTextBoxColumn.DataPropertyName = "Курб";
+            this.курбDataGridViewTextBoxColumn.HeaderText = "Курб";
+            this.курбDataGridViewTextBoxColumn.Name = "курбDataGridViewTextBoxColumn";
+            // 
+            // натичаDataGridViewTextBoxColumn
+            // 
+            this.натичаDataGridViewTextBoxColumn.DataPropertyName = "Натича";
+            this.натичаDataGridViewTextBoxColumn.HeaderText = "Натича";
+            this.натичаDataGridViewTextBoxColumn.Name = "натичаDataGridViewTextBoxColumn";
+            // 
+            // санаDataGridViewTextBoxColumn
+            // 
+            this.санаDataGridViewTextBoxColumn.DataPropertyName = "Сана";
+            this.санаDataGridViewTextBoxColumn.HeaderText = "Сана";
+            this.санаDataGridViewTextBoxColumn.Name = "санаDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(754, 493);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Қурби Асъор";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exchangeDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exchangeTabBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private exchangeDataSet exchangeDataSet;
+        private System.Windows.Forms.BindingSource exchangeTabBindingSource;
+        private exchangeDataSetTableAdapters.exchangeTabTableAdapter exchangeTabTableAdapter;
+        private exchangeDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn номиАсъорDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn маблагDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn асъориИвазDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn курбDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn натичаDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn санаDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ exchangeRate/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//conectSQL
+
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace exchangeRate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String connectionData = @"Data Source=.;Initial Catalog=exchange;Integrated Security=true";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayDB();
+            //Application.StartupPath
+        }
+
+        private void DisplayDB()
+        {
+            String query = "select * from exchangeTab";
+            using (SqlConnection connection = new SqlConnection(connectionData))
+            {
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "exchangeTab");
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double jj = Convert.ToDouble(textBox2.Text) * Convert.ToDouble(textBox4.Text);
+            textBox5.Text = Convert.ToString(jj);
+            string query = "Insert into exchangeTab Values('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + textBox5.Text + "',GETDATE())";
+            using (SqlConnection connection = new SqlConnection(connectionData))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+
+            }
+            DisplayDB();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            DisplayDBS(textBox6.Text.Trim(), textBox7.Text.Trim(), textBox8.Text.Trim());
+        }
+
+        private void DisplayDBS(string searchText0, string searchText1, string searchText2)
+        {
+            String query = string.Format("select * from exchangeTab where day(Сана) like '{0}%' and month(Сана) like '{1}%' and year(Сана) like '{2}%'", searchText0, searchText1, searchText2);
+            using (SqlConnection connection = new SqlConnection(connectionData))
+            {
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "exchangeTab");
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+        }
+    }
+}
